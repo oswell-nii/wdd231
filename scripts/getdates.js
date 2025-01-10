35cff7bf369cf28b19a788b1f7f6ffbbc1befc06
oswell-oxy <oswellokine1999@gmail.com>
2025-01-10 16:38:18 +0000

    First Commit for week 1 project

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const now = new Date();
+const year = now.getFullYear();
+
+document.getElementById('lastModified').textContent = document.lastModified;
+document.getElementById('currentYear').textContent = year;
